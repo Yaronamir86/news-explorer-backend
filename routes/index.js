7ce80ce717a7f0b86726fae5de8164c55c7a3313
yaron amir <yaronamir86@gmail.com>
2022-10-31 00:04:02 +0200

    starting from top

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const nonRoute = require('./nonExist');
-
-router.use('*', nonRoute);
-
-module.exports = router;
--- routes/nonExist.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const NotFound = require('../errors/NotFound-err');
-
-// eslint-disable-next-line no-unused-vars
-router.all('*', (req, res, next) => {
-  next(new NotFound('required resource not found'));
-});
-
-module.exports = router;
