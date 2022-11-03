d4b4cb8a6b902e6cb8c179a897389cf54c00e97e
yaron amir <yaronamir86@gmail.com>
2022-11-03 08:43:52 +0200

    fix routes issue

--- routes/nonExist.js
+++ routes/nonExist.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const NotFound = require('../errors/NotFound-err');
 
 // eslint-disable-next-line no-unused-vars
-router.all('*', (req, res, next) => {
+router.use('*', (req, res, next) => {
   next(new NotFound('required resource not found'));
 });
 
